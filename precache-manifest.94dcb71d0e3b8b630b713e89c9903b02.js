177e420d760912f1def75acb5d1aa5dea43fc466
Bradley Jensen <bradley13@gmail.com>
2019-05-04 10:31:11 -0400

    Updates

---
+++ precache-manifest.94dcb71d0e3b8b630b713e89c9903b02.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75fb71532a4888848db7ee60bf40ff74",
+    "url": "/clicky-memory-game/index.html"
+  },
+  {
+    "revision": "6a4a579ab4ef318a0293",
+    "url": "/clicky-memory-game/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "eb5d03ae7c9a30028659",
+    "url": "/clicky-memory-game/static/js/2.4e81ba5b.chunk.js"
+  },
+  {
+    "revision": "6a4a579ab4ef318a0293",
+    "url": "/clicky-memory-game/static/js/main.caf92a5b.chunk.js"
+  },
+  {
+    "revision": "2d83433aa8872a544556",
+    "url": "/clicky-memory-game/static/js/runtime~main.83671481.js"
+  }
+]);
