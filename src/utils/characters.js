f3759f5526892243aa796aeb2ce7ec42ec8f79e3
Bradley Jensen <bradley13@gmail.com>
2019-05-04 10:40:17 -0400

    fixing images in build

--- src/utils/characters.js
+++ src/utils/characters.js
@@ -3,62 +3,62 @@ export default {
     {
       id: 0,
       name: "Jon Snow",
-      image: "./images/jon.jpg",
+      image: "/images/jon.jpg",
     },
     {
       id: 1,
       name: "Daenerys Targaryen",
-      image: "./images/daenerys.jpg",
+      image: "/images/daenerys.jpg",
     },
     {
       id: 2,
       name: "Sansa Stark",
-      image: "./images/sansa.jpg",
+      image: "/images/sansa.jpg",
     },
     {
       id: 3,
       name: "Arya Stark",
-      image: "./images/arya.jpg",
+      image: "/images/arya.jpg",
     },
     {
       id: 4,
       name: "Tyrion Lannister",
-      image: "./images/tyrion.jpg",
+      image: "/images/tyrion.jpg",
     },
     {
       id: 5,
       name: "Jamie Lannister",
-      image: "./images/jamie.jpg",
+      image: "/images/jamie.jpg",
     },
     {
       id: 6,
       name: "Cersei Lannister",
-      image: "./images/cersei.jpg",
+      image: "/images/cersei.jpg",
     },
     {
       id: 7,
       name: "Samwell Tarly",
-      image: "./images/samwell.jpg",
+      image: "/images/samwell.jpg",
     },
     {
       id: 8,
       name: "Brienne of Tarth",
-      image: "./images/brienne.jpg",
+      image: "/images/brienne.jpg",
     },
     {
       id: 9,
       name: "Jorah Mormont",
-      image: "./images/jorah.jpg",
+      image: "/images/jorah.jpg",
     },
     {
       id: 10,
       name: "Davos Seaworth",
-      image: "./images/davos.jpg",
+      image: "/images/davos.jpg",
     },
     {
       id: 11,
       name: "Petyr Baelish",
-      image: "./images/peter.jpg",
+      image: "/images/peter.jpg",
     },
   ]
 }
