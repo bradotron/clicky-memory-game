6690c3d823277be5f6126c9a924b0b94cc5f1661
Bradley Jensen <bradley13@gmail.com>
2019-06-26 17:42:11 -0700

    minor update to GameCard

--- src/components/GameCard/index.js
+++ src/components/GameCard/index.js
@@ -2,12 +2,19 @@ import React from "react";
 
 function GameCard(props) {
   let imgStyle = {
-    width: "120px",
-    height: "120px",
+    width: "100%",
+    height: "auto",
   };
 
+  let imgContainer = {
+    width: "33%",
+  }
+
   return (
-    <img data-id={props.id} src={process.env.PUBLIC_URL + props.image} className="m-2" style={imgStyle} alt={props.name} {...props} />
+    // <img data-id={props.id} src={process.env.PUBLIC_URL + props.image}  style={imgStyle} alt={props.name} {...props} />
+    <div style={imgContainer}>
+      <img data-id={props.id} src="https://placehold.it/150x150" className="border" style={imgStyle} alt={props.name} {...props} />
+    </div>
   );
 }
 
